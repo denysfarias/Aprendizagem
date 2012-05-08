58b8bc8d42ddae868fee68315b93fd31a6e4ecb6
Denys F <denys.lf@gmail.com>
2012-05-08 10:25:23 -0300

    Code correction - DONE

--- matlab/kmeansErrorCalculation.m
+++ matlab/kmeansErrorCalculation.m
@@ -2,38 +2,42 @@
 [dataset, datasetClasses, trainIndices, testIndices] = createDataset(0.7);
 
 % Apply k-means 100 times for 2 clusters
-[idx, centroids] = kmeans(dataset, 2, 'replicates', 100);
+[kmeansIndices, kmeansCentroids] = kmeans(dataset, 2, 'replicates', 100);
 
 % Plot clusters
 disp('%Plotando os clusters identificados.');
 figure;
-gscatter(dataset(:,1), dataset(:,2), idx, 'rbg', 'o', 5, 0);
+gscatter(dataset(:,1), dataset(:,2), kmeansIndices, 'rbg', 'o', 5, 0);
 title('Clusters', 'FontWeight','Bold','FontSize',14);
 drawnow;
 
 % Define classes centroids
-classesCentroid = [mean(dataset(datasetClasses == 1, :));
+classesCentroids = [mean(dataset(datasetClasses == 1, :));
     mean(dataset(datasetClasses == 2, :))]
 
 % Define clusters classes
-clustersClasses = knnsearch(classesCentroid, centroids);
+clustersClasses = knnsearch(classesCentroids, kmeansCentroids);
 
 % Find classification by centroids
-c1IdxCentroids = knnsearch(centroids, dataset(datasetClasses == 1, :));
-c1IdxCentroids(c1IdxCentroids == 1) = classesCentroid(1);
-c1IdxCentroids(c1IdxCentroids == 2) = classesCentroid(2);
+c1ClassificationByCentroids = knnsearch(kmeansCentroids, dataset(datasetClasses == 1, :));
+c1ClassificationByCentroids(c1ClassificationByCentroids == 1) = clustersClasses(1);
+c1ClassificationByCentroids(c1ClassificationByCentroids == 2) = clustersClasses(2);
 
-c2IdxCentroids = knnsearch(centroids, dataset(datasetClasses == 2, :));
-c2IdxCentroids(c2IdxCentroids == 1) = classesCentroid(1);
-c2IdxCentroids(c2IdxCentroids == 2) = classesCentroid(2);
+c2ClassificationByCentroids = knnsearch(kmeansCentroids, dataset(datasetClasses == 2, :));
+c2ClassificationByCentroids(c2ClassificationByCentroids == 1) = clustersClasses(1);
+c2ClassificationByCentroids(c2ClassificationByCentroids == 2) = clustersClasses(2);
+
+classificationByCentroids = kmeansIndices;
+classificationByCentroids(classificationByCentroids == 1) = clustersClasses(1);
+classificationByCentroids(classificationByCentroids == 2) = clustersClasses(2);
 
 % Calculate error per class
-nC1CorrectlyAssigned = sum(c1IdxCentroids == 1);
-nC2CorrectlyAssigned = sum(c2IdxCentroids == 2);
+nC1CorrectlyAssigned = sum(c1ClassificationByCentroids == 1);
+nC2CorrectlyAssigned = sum(c2ClassificationByCentroids == 2);
 
-pC1Error     = 1 - nC1CorrectlyAssigned/size(c1IdxCentroids);
-pC2Error     = 1 - nC2CorrectlyAssigned/size(c2IdxCentroids);
+pC1Error     = 1 - nC1CorrectlyAssigned/size(c1ClassificationByCentroids,1);
+pC2Error     = 1 - nC2CorrectlyAssigned/size(c2ClassificationByCentroids,1);
 pGlobalError = 1- (nC1CorrectlyAssigned + nC2CorrectlyAssigned)/size(dataset,1);
 
 % Calculate Rand
-
+adjustedRandIndex = RandIndex(kmeansIndices, datasetClasses);
